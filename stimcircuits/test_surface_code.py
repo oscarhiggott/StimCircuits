654846372f2c74a1171ba6f9a1745928f6d4dbd1
francois-marie <fm.le.regent@gmail.com>
2022-12-08 15:33:06 +0100

    test generated circuit graphlike distance with distance x_distance and z_distance supplied

--- stimcircuits/test_surface_code.py
+++ stimcircuits/test_surface_code.py
@@ -32,6 +32,22 @@ gen_test_params_surface_code = [
     ("surface_code:rotated_memory_z", 5, 1, 0.1, 0.002, 0.003, 0.004),
     ("surface_code:rotated_memory_z", 2, 2, 0, 0.01, 0, 0)
 ]
+gen_test_params_distances_surface_code = [
+    ("surface_code:unrotated_memory_x", 3, 3, 3, 10, 0.001, 0.002, 0.003,
+     0.004),
+    ("surface_code:unrotated_memory_x", 5, 3, 5, 1, 0.1, 0.002, 0.003, 0.004),
+    ("surface_code:unrotated_memory_x", 2, 2, 2, 2, 0, 0.01, 0, 0),
+    ("surface_code:unrotated_memory_z", 3, 5, 3, 2, 0.001, 0.002, 0.003, 0.004),
+    ("surface_code:unrotated_memory_z", 5, 5, 5, 1, 0.1, 0.002, 0.003, 0.004),
+    ("surface_code:unrotated_memory_z", 2, 2, 2, 2, 0, 0.01, 0, 0),
+    ("surface_code:rotated_memory_x", 3, 5, 3, 10, 0.001, 0.002, 0.003, 0.004),
+    ("surface_code:rotated_memory_x", 5, 3, 5, 1, 0.1, 0.002, 0.003, 0.004),
+    ("surface_code:rotated_memory_x", 2, 2, 2, 2, 0, 0.01, 0, 0),
+    ("surface_code:rotated_memory_z", 3, 5, 3, 2, 0.001, 0.002, 0.003, 0.004),
+    ("surface_code:rotated_memory_z", 5, 3, 5, 1, 0.1, 0.002, 0.003, 0.004),
+    ("surface_code:rotated_memory_z", 2, 2, 2, 2, 0, 0.01, 0, 0)
+]
+
 
 
 @pytest.mark.parametrize(
@@ -109,6 +125,39 @@ def test_generated_circuit_graphlike_distance(
         shortest_error = dem.shortest_graphlike_error()
         assert len(shortest_error) == distance
 
+@pytest.mark.parametrize(
+    "code_task,distance,x_distance,z_distance,rounds,after_clifford_depolarization,"
+    "before_round_data_depolarization,"
+    "before_measure_flip_probability,after_reset_flip_probability",
+    gen_test_params_distances_surface_code
+)
+def test_generated_circuit_with_distances_graphlike_distance(
+        code_task: str,
+        distance: int,
+        x_distance: int,
+        z_distance: int,
+        rounds: int,
+        after_clifford_depolarization: float,
+        before_round_data_depolarization: float,
+        before_measure_flip_probability: float,
+        after_reset_flip_probability: float
+) -> None:
+    for b in (False, True):
+        circuit = generate_circuit(
+            code_task=code_task,
+            distance=distance,
+            x_distance=x_distance,
+            z_distance=z_distance,
+            rounds=rounds,
+            after_clifford_depolarization=after_clifford_depolarization,
+            before_round_data_depolarization=before_round_data_depolarization,
+            before_measure_flip_probability=before_measure_flip_probability,
+            after_reset_flip_probability=after_reset_flip_probability,
+            exclude_other_basis_detectors=b
+        )
+        dem = circuit.detector_error_model(decompose_errors=True)
+        shortest_error = dem.shortest_graphlike_error()
+        assert len(shortest_error) == distance
 
 @pytest.mark.parametrize(
     "code_task,distance,rounds,after_clifford_depolarization,before_round_data_depolarization,"
